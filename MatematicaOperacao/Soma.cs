edc2be1da14c5284a98589b2818726f980f1264c
Samuel Soares Guimarães <34168199+samuelsoares27@users.noreply.github.com>
2023-08-22 11:31:16 -0300

    Add project files.

---
+++ MatematicaOperacao/Soma.cs
@@ -0,0 +1,10 @@
+﻿namespace MatematicaOperacao
+{
+    public class Soma
+    {
+        public static double Somar(double x1, double x2)
+        {
+            return x1 + x2;
+        }
+    }
+}
